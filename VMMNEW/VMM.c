d0b2906e41c66c28b218b778d4fb28e903db56cb
nightmare-man <107lsmlsm@whut.edu.cn>
2024-06-05 11:29:19 +0800

    vmcs setup

--- VMMNEW/VMM.c
+++ VMMNEW/VMM.c
@@ -143,6 +143,14 @@ BOOLEAN vm_ptr_load(struct VMM_STATE* vmm_state_ptr) {
 	}
 	return TRUE;
 }
+BOOLEAN setup_vmcs(struct VMM_STATE* vmm_state_ptr, union EXTENDED_PAGE_TABLE_POINTER* eptp) {
+	__vmx_vmwrite(GUEST_CR0, __readcr0());
+	__vmx_vmwrite(GUEST_CR3, __readcr3());
+	__vmx_vmwrite(GUEST_CR4, __readcr4());
+	__vmx_vmwrite(GUEST_DR7, 0x400ULL);//硬件调试寄存器
+	__vmx_vmwrite(GUEST_RSP, (vmm_state_ptr->vmm_stack + VMM_STACK_SIZE - 1));
+	__vmx_vmwrite(GUEST_RIP, (ULONG64)1);//这里需要都是虚拟地址，因为cr3中开启了分页
+}
 BOOLEAN launch_vmx(struct VMM_STATE* vmm_state_ptr, int process_id, union EXTENDED_PAGE_TABLE_POINTER* eptp) {
 	KdPrint(("----launch vmx----\n"));
 	KAFFINITY ka = 2 ^ process_id;
@@ -153,15 +161,35 @@ BOOLEAN launch_vmx(struct VMM_STATE* vmm_state_ptr, int process_id, union EXTEND
 	//为VMM分配栈，理论上，开启VMXON就是在VMM了，用的栈是当前的ss rsp，
 	//但是我们希望VMM使用新栈，因此重新分配一个，放在vmcs的host data中，当第一次vm exit后，
 	//使用新分配的作为rsp
-	//需要注意的是，这里需要的vmm_stack是虚拟地址
+	//需要注意的是，这里需要的vmm_stack 是vmm里的虚拟地址
+	//我们的vmm是windows环境下的驱动程序，因此使用的cr3是系统进程的cr3
+	//因此这里的虚拟地址就是allocate分配下来的地址。
+	//假设我们进入guest后，继续执行剩余的代码，这个时候的windows就是guest了。
 
-	ULONG64 vmm_stack_va = ExAllocatePool(NonPagedPool, VMM_STACK_SIZE, DRIVER_TAG);
+	ULONG64 vmm_stack_va = ExAllocatePoolWithTag(NonPagedPool, VMM_STACK_SIZE, DRIVER_TAG);
 	if (vmm_stack_va==NULL) {
 		KdPrint(("allocate vmm stack fail\n"));
 		return FALSE;
 	}
 	RtlZeroMemory(vmm_stack_va, VMM_STACK_SIZE);
-	vmm_state_ptr->vmm_stack = vitual_to_physical(vmm_stack_va);
+	vmm_state_ptr->vmm_stack = vmm_stack_va;
 
+	//分配msrbitmap,根据intel手册， vmcs里保存一个msrbitmap物理内存指针
+	//指向一个4kb的区域
+	ULONG64 msr_bitmap = ExAllocatePoolWithTag(NonPagedPool, PAGE_SIZE, DRIVER_TAG);
+	if (msr_bitmap == NULL) {
+		KdPrint(("allocate msr bitmaps fail\n"));
+		return FALSE;
+	}
+	RtlZeroMemory(msr_bitmap, PAGE_SIZE);
+	vmm_state_ptr->msr_bitmaps_va = msr_bitmap;
+	vmm_state_ptr->msr_bitmaps_pa = vitual_to_physical(msr_bitmap);
+
+	if (!clear_vm_state(vmm_state_ptr)) {
+		return FALSE;
+	}
+	if (!vm_ptr_load(vmm_state_ptr)) {
+		return FALSE;
+	}
 
 }
